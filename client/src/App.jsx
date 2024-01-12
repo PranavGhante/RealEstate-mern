894f1588fa3cffbe06b4558b1b3ecb0b720ec447
Pranav <pranavghante28@gmail.com>
2024-01-12 21:07:44 +0530

    change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Signin from './pages/SignIn'
+import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
